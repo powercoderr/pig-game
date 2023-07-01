0f78282ad56ff505d38efa1203558758e97d857a
powercoderr <djayusmandena@gmail.com>
2023-07-01 09:19:21 +0700

    feat: reset game state by new game button

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//DOM Objects
+const newBtn = document.querySelector('.btn--new');
+
+//Function to handle event
+const gameReset = function () {
+  //Set score & current-score to 0
+  document.querySelectorAll('.score').forEach(element => {
+    element.textContent = 0;
+  });
+
+  document.querySelectorAll('.current-score').forEach(element => {
+    element.textContent = 0;
+  });
+
+  //Set player--0 as active player
+  const firstPlayer = document.querySelector('.player--0').classList;
+  const secondPlayer = document.querySelector('.player--1').classList;
+  if (!firstPlayer.contains('player--active')) {
+    firstPlayer.add('player--active');
+  }
+
+  if (secondPlayer.contains('player--active')) {
+    secondPlayer.remove('player--active');
+  }
+};
+
+//Events
+newBtn.addEventListener('click', gameReset);
